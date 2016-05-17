33f9058007225dfe62008ab5fa97c5d4fbfd096a
sksakamoto <wondafury@gmail.com>
2016-05-16 23:06:10 -0700

    Update shopping_list.py

--- shopping_list.py
+++ shopping_list.py
@@ -15,10 +15,26 @@ def remove_item(item):
 		shopping_list.remove(item.lower())
 		print sorted_list(shopping_list)
 	else: print "This item is not on the list!"
+	
+def menu():
+	global menu_choice
+	menu_choice = raw_input("What would you like to do? 0 - Main Menu, 1 - Show Current List, 2 - Add an Item to Shopping List")
+	return menu_choice
 
 def main():
-	add_item("kale")
-	remove_item("kale")
+	while(True):
+		print "Type exit to exit"
+		menu()
+		if menu_choice == "exit":
+			break
+		elif menu_choice == 0:
+			None
+		elif menu_choice == 1:
+			print shopping_list
+		else:
+			add_item()
+#	add_item("kale")
+#	remove_item("kale")
 
 if __name__ == '__main__':
 	main()
