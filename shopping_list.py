71c21b8ab6aaceab40a9a96118b0c902e1f72ed5
Stephanie <wondafury@gmail.com>
2016-05-24 15:26:00 -0700

    added string parsing

--- shopping_list.py
+++ shopping_list.py
@@ -21,6 +21,14 @@ def menu():
 	menu_choice = raw_input("What would you like to do? 0 - Main Menu, 1 - Show Current List, 2 - Add an Item to List ")
 	return menu_choice
 
+def parsed_string(item_string):
+	new_items = item_string.split(",")
+	for item in new_items:
+		if item.lower() not in shopping_list:
+			shopping_list.append(item.lower())
+			print sorted_list(shopping_list)
+		else: print "This item is already on the list!"
+
 def main():
 	while(True):
 		print "Type exit to exit"
@@ -31,10 +39,13 @@ def main():
 			None
 		elif menu_choice == 1:
 			print shopping_list
+		elif menu_choice == 2:
+			input_answer = raw_input("What do you want to add? ")
+			if "," in input_answer:
+				parsed_string(input_answer)
+			else: add_item(input_answer)
 		else:
-			add_item(raw_input("What do you want to add? "))
-#	add_item("kale")
-#	remove_item("kale")
+			print "Sorry, I don't understand."
 
 if __name__ == '__main__':
 	main()
